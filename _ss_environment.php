2f5ed364dd4c665c2322aadbd0104dab4820ed61
Will Morgan <me@willmorgan.co.uk>
2014-10-24 15:18:36 +0100

    SS Environment

---
+++ _ss_environment.php
@@ -0,0 +1,22 @@
+<?php
+
+//define DB settings
+define('SS_DATABASE_SERVER', '127.0.0.1');
+define('SS_DATABASE_CLASS','MySQLDatabase');
+define('SS_DATABASE_TIMEZONE','+00:00');
+define('SS_DATABASE_USERNAME', 'root');
+define('SS_DATABASE_PASSWORD', '');
+define('SS_DATABASE_NAME', 'vagrant');
+
+//set the DB name - this provide backwards compatibility with 2.x and 3.0 sites
+global $database;
+$database = SS_DATABASE_NAME;
+
+//define('SS_DATABASE_SUFFIX', '_dev');
+define('SS_ENVIRONMENT_TYPE', 'dev');
+
+define('SS_DEFAULT_ADMIN_USERNAME', 'admin');
+define('SS_DEFAULT_ADMIN_PASSWORD', 'password');
+
+global $_FILE_TO_URL_MAPPING;
+$_FILE_TO_URL_MAPPING['/vagrant/www'] = 'http://localhost';
