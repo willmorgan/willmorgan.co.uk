51cfacbd01c02942c5c8e937c2071417fb09761b
Will Morgan <me@willmorgan.co.uk>
2014-12-31 00:41:18 +0000

    rev-parse is better than describe --all --long

--- www/hash.php
+++ www/hash.php
@@ -4,6 +4,6 @@
  * Get the first 8 characters of the git sha1 hash
  * Useful for quick and dirty cache busting for assets
  */
-$version = exec('git describe --all --long');
-$hash = substr($version, -8);
+$version = exec('git rev-parse HEAD');
+$hash = substr($version, 0, 8);
 return $hash;
