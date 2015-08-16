2ed0a38bb3b7339e00241d76a1059e68f153792d
Will Morgan <me@willmorgan.co.uk>
2015-08-16 19:08:44 +0100

    Minify HTML; update availability

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
+/* global require */
 var gulp = require('gulp');
 var minifyCSS = require('gulp-minify-css');
 var autoprefix = require('gulp-autoprefixer');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var htmlmin = require('gulp-htmlmin');
+var htmlreplace = require('gulp-html-replace');
 
 gulp.task('styles', function() {
 	gulp.src(
@@ -23,9 +26,9 @@ gulp.task('styles', function() {
 			'last 3 and_ff versions',
 		]
 	}))
-	.pipe(concat('./www/css/_combined.css'))
+	.pipe(concat('css/_combined.css'))
 	.pipe(minifyCSS())
-	.pipe(gulp.dest('.'));
+	.pipe(gulp.dest('www/dist'));
 });
 
 gulp.task('scripts', function() {
@@ -36,12 +39,36 @@ gulp.task('scripts', function() {
 			'./www/js/main.js'
 		]
 	)
-	.pipe(concat('./www/js/_combined.js'))
+	.pipe(concat('js/_combined.js'))
 	.pipe(uglify())
-	.pipe(gulp.dest('.'));
+	.pipe(gulp.dest('www/dist'));
+});
+
+gulp.task('html', function() {
+    var git = require('git-rev');
+    git.short(function(hash) {
+        gulp.src(
+            [
+                './www/*.html'
+            ]
+        )
+        .pipe(htmlreplace({
+                css: 'css/_combined-'+hash+'.css',
+                js: 'js/_combined-'+hash+'.js'
+        }))
+        .pipe(htmlmin({
+            collapseWhitespace: true,
+            minifyJS: true,
+            minifyCSS: true,
+            removeComments: true
+        }))
+        .pipe(gulp.dest('www/dist'));
+    });
 });
 
 gulp.task('default', ['styles', 'scripts']);
+
+gulp.task('build', ['styles', 'scripts', 'html']);
 
 gulp.task('watch', function() {
 	gulp.watch(['./www/css/all.css', './www/css/stripes.css'], ['styles']);
--- www/hash.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/**
- * Get the first 8 characters of the git sha1 hash
- * Useful for quick and dirty cache busting for assets
- */
-$version = exec('git rev-parse HEAD');
-$hash = substr($version, 0, 8);
-return $hash;
